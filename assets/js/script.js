3d094f23122947ba3fea80dd0f44a2cd6c72446d
Joseph <josephgabrieltina@gmail.com>
2022-03-28 16:53:07 +0800

    added scroll animations and info sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-let bars = document.getElementsByClassName('fa-bars')
-console.log(bars[0])
-
-function myfunc(){
-    
-}
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 50;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
